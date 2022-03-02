a11315fa33a2f06d0dbe8d07b7e00ee7c094e6f4
Topper Robotics <user@jcschools.org>
2022-03-02 15:53:59 -0500

    add motors

--- FRC-2022/src/main/java/frc/robot/Robot.java
+++ FRC-2022/src/main/java/frc/robot/Robot.java
@@ -15,7 +15,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
