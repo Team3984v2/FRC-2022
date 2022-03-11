1e669d955b5f99e8891605ad03d2fac89cc222c4
Topper Robotics <user@jcschools.org>
2022-03-11 16:24:19 -0500

    Create Constants.java

---
+++ FRC-2022/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,35 @@
+package frc.robot;
+
+
+
+
+
+/**
+ * Class for Constants
+ * <p>
+ * 
+ */
+public class Constants{
+
+    
+  //Drivetrain constants
+  public final int kleftRearMotorChannel = 0;
+  public final int krightRearMotorChannel = 1;
+  public final int kleftFrontMotorChannel = 2;
+  public final int krightFrontMotorChannel = 3;
+
+  public final int kControllerChannel = 0;
+
+  //TODO - set channels for solenoids
+  public final int kRightSolenoidChannel1 = 1;
+  public final int kRightSolenoidChannel2 = 0;
+  public final int kLeftSolenoidChannel1 = 1;
+  public final int kLeftSolenoidChannel2  = 0;
+
+  //Spark channels
+  public final int kintakeMotorChannel = 0;
+
+
+
+  
+}
